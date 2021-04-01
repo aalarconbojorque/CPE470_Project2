de02bbe06773d36b98f955328054b3420163517e
aalarconbojorque <aa.alarcon10@gmail.com>
2021-04-01 10:10:20 -0700

    Inital commit

---
+++ proj2.py
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------------------------
+# FILE NAME:         proj2.py
+# USAGE:             python3 proj2.py
+# NOTES:             Requires NumPy installation
+#                    Requires Python3
+#
+# MODIFICATION HISTORY:
+# Author             Date           Modification(s)
+# ----------------   -----------    ---------------
+# Andy Alarcon       04-01-2021     1.0 ... setup dev environment, imported NumPy
+# -----------------------------------------------------------------------------
+
+import numpy.matlib as m
+import numpy as np
+import math
+import random
+
+
+def main():
+
+    print("Hello World")
+    
+
+    
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
