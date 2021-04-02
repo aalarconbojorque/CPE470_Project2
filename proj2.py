68a22e9bde4b69a0d69450ddc21f0d6c65e6ac85
aalarconbojorque <aa.alarcon10@gmail.com>
2021-04-02 11:25:43 -0700

    not done

--- proj2.py
+++ proj2.py
@@ -14,6 +14,7 @@ import numpy.matlib as m
 import numpy as np
 import math
 import random
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -51,23 +52,83 @@ def main():
     noise_std = 0.5
 
     #Set Virtual Target Data
-    for index, item in enumerate(qrv, start=0):   
+    for index, item in enumerate(qv, start=0):   
         if index == 0:
             pass
         else :
             #Set circular target tragectory (position)
             qv_x = 60 - 15 * np.cos(t[index])
             qv_y = 30 + 15 * np.sin(t[index])
-            qrv[index] = np.array([qv_x, qv_y]).reshape(2,)
+            qv[index] = np.array([qv_x, qv_y]).reshape(2,)
 
             #Set heading of virtual target
             theta_t[index] = np.arctan2(qv_y, qv_x)
 
 
-            print(qrv[index], theta_t[index], index)
+            #print(qv[index][1], theta_t[index][0], index)
+    
+    #Compute relative data
+    for index, item in enumerate(qrv, start=0):   
+        if index == 0:
+            pass
+        else :
+            #Compute relative y pos V - R
+            yrv = qv[index][1] - qr[index][1]
+            #Compute relative x pos V - R
+            xrv = qv[index][0] - qr[index][0]
+            #Compute relative heading
+            Hrv = np.arctan2(yrv, xrv)
+            #Compute relative position
+            qrv[index] = qv[index] - qr[index]
+            
+            #Compute velocity of the robot
+            V_Left = CompMagSqr(pv)
+            V_Middle = 2*Lambda*CompMag(qrv[index])*CompMag(pv)*CosSub(theta_t[index], Hrv)
+            V_Right = np.square(Lambda)*CompMagSqr(qrv[index])
+            pr[index] = np.sqrt(V_Left + V_Middle + V_Right)
+
+            #Computer orientation of robot
+            arcSin = (CompMag(pv)*np.sin(theta_r[index]- Hrv)) / CompMag(pr[index])
+            theta_r[index] = Hrv + np.arcsin(arcSin)
+
+            #Update position of robot 
+            # theta_r[index-1]
+            qr[index] = qr[index - 1] + pr[index] * delta_t * np.array([np.cos(theta_r[index-1]), np.sin(theta_r[index-1])]).reshape(2,)
+            prv[index] = pv - pr[index]
+
+
+            #print(qv[index][1], theta_t[index][0], index)
+
+
+    f = open("output.txt", "a+")
+    
+    for index, item in enumerate(qrv, start=0):   
+        f.write(str(qrv[index][0]) + '|' + str(qrv[index][1]) + '|' + str(theta_r[index][0]) + '\n')
+    
+    f.close()   
+
+            
+
+  
     print(prv[0])
 
 
+def CompMagSqr(vec):
+    #Compute magnitued
+    vec = np.linalg.norm(vec)
+    #Compute squared
+    vec = np.square(vec)
+    return vec
+
+def CompMag(vec):
+    #Compute magnitued
+    vec = np.linalg.norm(vec)
+    return vec
+
+def CosSub(theta_1, theta_2):
+    theta_3 = np.cos(theta_1 - theta_2)
+    theta_3 = np.absolute(theta_3)
+    return theta_3
     
 
     
